f77148464149a8eeac1352fb978c37182d78695c
damjan123456 <74983031+damjan123456@users.noreply.github.com>
2025-10-05 17:05:58 +0200

    Add files via upload

---
+++ ZAJEDNICKI/src/komunikacija/Operacija.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package komunikacija;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author damja
+ */
+public enum Operacija implements Serializable {
+    LOGIN, 
+    OBRISI_CITAOCA, 
+    UNESI_SERTIFIKAT, 
+    VRATI_CITAOCE, 
+    VRATI_MESTA, 
+    UNESI_CITAOCA, 
+    IZMENI_CITAOCA, 
+    VRATI_ZAPISE, 
+    VRATI_STAVKE, 
+    VRATI_KNJIGE, 
+    KREIRAJ_ZAPIS, 
+    IZMENI_ZAPIS, 
+    VRATI_BIBLIOTEKARE
+}
